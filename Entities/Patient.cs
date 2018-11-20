9d52b0e8bbd4814b0c551abe8c35e49c707724a3
Daniel Ubilla Acuña <dubillaa477@ulacit.ed.cr>
2018-11-19 20:14:38 -0600

    Patient entitie

---
+++ Entities/Patient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Patient
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string TipoSangre { get; set; }
+        public ConsultationRecord consulRecord { get; set; }
+        public TestHistory TestHistory { get; set; }
+        public ActiveMedication activeMedication { get; set; } 
+
+
+    }
+}
