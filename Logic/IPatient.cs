9218306dff5e514e4b9a300001d780d878afb867
Daniel Ubilla Acuña <dubillaa477@ulacit.ed.cr>
2018-11-19 20:33:01 -0600

    a

--- Logic/IPatient.cs
+++ Logic/IPatient.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace Logic
 {
-    public class IPatient
+    public interface IPatient
     {
+        List<Patient> GetPatients();
     }
 }
